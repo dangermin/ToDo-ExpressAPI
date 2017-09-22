8a0ac08c1b44fc6069c06414bfe87baefc7f3090
Dangermin <dangermin.field@gamil.com>
2017-09-21 21:15:24 -0700

    added GETs

--- server/app.js
+++ server/app.js
@@ -1,38 +1,44 @@
 const express = require('express');
 const morgan = require('morgan');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const _ = require('underscore');
+
 const app = express();
 
-let mockData = [
-  {
-    todoItemId: 0,
-    name: 'an item',
-    priority: 3,
-    completed: false
-  },
-  {
-    todoItemId: 1,
-    name: 'another item',
-    priority: 2,
-    completed: false
-  },
-  {
-    todoItemId: 2,
-    name: 'a done item',
-    priority: 1,
-    completed: true
-  }
-];
-
-//Middleware
+// Middleware
 app.use(morgan('dev'));
 
+app.use(bodyParser.json());
+
+// Check file access
+fs.access('server/todos.json',fs.constants.R_OK | fs.constants.W_OK, (err) => {
+  console.log(err ? 'no access!' : 'can read/write');
+});
 // add your code here
+const todos = (require( "./todos.json"));
+
+
 app.get('/', (req, res) => {
   res.status(200).send({
     status: 'OK'
-  });
+  })
+});
+//GET TODOS
+app.get('/todos', (req, res) => {
+  res.json(todos);
+});
+//GET single TODO
+app.get('/todos/:id', (req, res) => {
+  // console.log(todos);
+  var todoItemId = parseInt(req.params.id);
+  var matchedTodo = _.findWhere(todos, {todoItemId: todoItemId});
+  // console.log(matchedTodo);
+  if (matchedTodo){
+    res.json(matchedTodo);
+  }
 })
 
-module.exports = app;
 
+module.exports = app;
 
